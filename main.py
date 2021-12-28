07b5c07a7a0c9225c8a7fe99d99ba988729cf290
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-12-28 15:57:55 +0900

    경로 수정2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from python.serial.SerialManager import SerialManager
 
 if __name__ == "__main__":
     import sys
+    sys.path.append("/home/pi/Documents/TempDataLogger-")
     sys.path.append("/home/pi/Documents/TempDataLogger-/python/serial")
     serial = SerialManager()
     serial.start()
