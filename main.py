5275c1239c649b49526f05d0fdad7c98aa4623ee
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-12-30 15:51:39 +0900

    gui 7

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from python.serial.SerialManager import SerialManager
-from guizero import App, Text, TextBox, PushButton
+from guizero import App, Text, TextBox, PushButton, Slider
 
 def say_my_name():
     welcome_message.value = my_name.value
+
+def change_text_size(slider_value):
+    welcome_message.size = slider_value
 
 if __name__ == "__main__":
     import sys
@@ -17,5 +20,6 @@ if __name__ == "__main__":
     welcome_message = Text(app, text="Welcome to my app", size=40, font="Times New Roman", color="lightblue")
     my_name = TextBox(app, width=40)
     update_text = PushButton(app, command=say_my_name, text="Display my name")
+    text_size = Slider(app, command=change_text_size, start=10, end=80)
 
     app.display()
