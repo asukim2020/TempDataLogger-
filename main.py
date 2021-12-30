d2055a00b8b5af394b96194ae640fff27599ac49
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-12-30 15:41:45 +0900

    gui 1

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from python.serial.SerialManager import SerialManager
+from guizero import App
 
 if __name__ == "__main__":
     import sys
     sys.path.append("/home/pi/Documents/TempDataLogger-")
     sys.path.append("/home/pi/Documents/TempDataLogger-/python/serial")
 
-    serial = SerialManager()
-    serial.start()
+    # serial = SerialManager()
+    # serial.start()
+
+    app = App(title="Hello world")
+    app.display()
